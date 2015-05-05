31bd17cab8bb9b6793e7f145c9353f2a743d5bba
Mircea Bratan <mbratan@tremend.ro>
2015-05-05 16:24:57 +0300

    Import curl 7.41.0

---
+++ curl/tests/configurehelp.pm
@@ -0,0 +1,23 @@
+# This is a generated file.  Do not edit.
+
+package configurehelp;
+
+use strict;
+use warnings;
+use Exporter;
+
+use vars qw(
+    @ISA
+    @EXPORT_OK
+    $Cpreprocessor
+    );
+
+@ISA = qw(Exporter);
+
+@EXPORT_OK = qw(
+    $Cpreprocessor
+    );
+
+$Cpreprocessor = 'arm-linux-androideabi-gcc -E -DANDROID -isystem /storage/tudor/aosp/bionic/libc/arch-arm/include -isystem /storage/tudor/aosp/bionic/libc/include -isystem /storage/tudor/aosp/bionic/libc/kernel/common -isystem /storage/tudor/aosp/bionic/libc/kernel/arch-arm -isystem /storage/tudor/aosp/external/zlib -I/storage/tudor/aosp/external/openssl/include -I/storage/tudor/aosp/external/openssl/include/openssl';
+
+1;
